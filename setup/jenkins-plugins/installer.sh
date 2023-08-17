10c6ba078a3e96cf7a327bda071bcf74df648292
root <root@devsecops-cloud.a13kcwzlwvcefeg3ynswjzzyih.bx.internal.cloudapp.net>
2023-08-17 03:45:02 +0000

    passwords added

--- setup/jenkins-plugins/installer.sh
+++ setup/jenkins-plugins/installer.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -eo pipefail
+JENKINS_URL='http://aczjenkins.eastus.cloudapp.azure.com:8080/'
 
-JENKINS_URL='http://localhost:8080'
+JENKINS_CRUMB=$(curl -s --cookie-jar /tmp/cookies -u aczepiel:'G1i2z3z4m5o6h7()=' ${JENKINS_URL}/crumbIssuer/api/json | jq .crumb -r)
 
-JENKINS_CRUMB=$(curl -s --cookie-jar /tmp/cookies -u admin:admin ${JENKINS_URL}/crumbIssuer/api/json | jq .crumb -r)
-
-JENKINS_TOKEN=$(curl -s -X POST -H "Jenkins-Crumb:${JENKINS_CRUMB}" --cookie /tmp/cookies "${JENKINS_URL}/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken?newTokenName=demo-token66" -u admin:admin | jq .data.tokenValue -r)
+JENKINS_TOKEN=$(curl -s -X POST -H "Jenkins-Crumb:${JENKINS_CRUMB}" --cookie /tmp/cookies "${JENKINS_URL}/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken?newTokenName=demo-token66" -u aczepiel:'G1i2z3z4m5o6h7()=' | jq .data.tokenValue -r)
 
 echo $JENKINS_URL
 echo $JENKINS_CRUMB
@@ -14,7 +12,7 @@ echo $JENKINS_TOKEN
 
 while read plugin; do
    echo "........Installing ${plugin} .."
-   curl -s POST --data "<jenkins><install plugin='${plugin}' /></jenkins>" -H 'Content-Type: text/xml' "$JENKINS_URL/pluginManager/installNecessaryPlugins" --user "admin:$JENKINS_TOKEN"
+   curl -s POST --data "<jenkins><install plugin='${plugin}' /></jenkins>" -H 'Content-Type: text/xml' "$JENKINS_URL/pluginManager/installNecessaryPlugins" --user "aczepiel:$JENKINS_TOKEN"
 done < plugins.txt
 
 
